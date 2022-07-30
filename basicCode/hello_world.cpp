713ebafd8d75fce89cb90cffd42cf4acf254df34
w-zq <928671345@qq.com>
2022-07-31 01:41:41 +0800

    hello world

---
+++ basicCode/hello_world.cpp
@@ -0,0 +1,12 @@
+//a hello world program in C++
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "hello world!";
+	return 0;
+}
+
+
+// you can use g++ to run the this file, and you'll get a output file, then run it. like: g++ hello_world.cpp -o hello_world, and then run ./hello_world
