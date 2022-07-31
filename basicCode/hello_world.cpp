ce781af7e7d98f8707204630867149cfafaf9017
w-zq <928671345@qq.com>
2022-07-31 16:53:00 +0800

    some new script

--- basicCode/hello_world.cpp
+++ basicCode/hello_world.cpp
@@ -4,9 +4,16 @@ using namespace std;
 
 int main()
 {
-	cout << "hello world!";
+	cout << "hello world!\n";
+	cout << "I am an OpenFoam fan." << endl;
 	return 0;
 }
 
 
 // you can use g++ to run the this file, and you'll get a output file, then run it. like: g++ hello_world.cpp -o hello_world, and then run ./hello_world
+
+
+
+
+// cout means c out.
+// endl means end line.
